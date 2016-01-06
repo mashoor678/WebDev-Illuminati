b71db122c41bbfdcc58a312b2d9c99f4833a7db1
asankalhh <asankahendahewa@live.com>
2016-01-07 03:55:24 +0530

    First Upload Signed-off-by: asankalhh <asankahendahewa@live.com>

---
+++ Test000001/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test000001.Startup))]
+namespace Test000001
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
